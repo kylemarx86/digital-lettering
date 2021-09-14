a8fe82e444e203ed6183770d322caf1d3c1dd6f4
kylemarx86 <kylemarx86@csu.fullerton.edu>
2021-09-14 00:01:24 -0700

    created method to create character with all LEDs lit.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     make_char("A");
     make_char("B");
     make_char("X");
+    create_fully_lit_char("B");
 });
 
 
@@ -56,5 +57,72 @@ function make_char(char_to_write){
         $('.char_X .led11').addClass('unlit');
         $('.char_X .led13').addClass('unlit');
         $('.char_X .led15').addClass('unlit');
+    }
+}
+
+/**
+ * param:
+ */
+ function create_fully_lit_char(char_to_write){
+    var char_class = "char_" + char_to_write;
+    var $char = $('<div>').addClass('char').addClass(char_class);
+    $('.container').append($char);
+
+    //create outer LEDs in character
+    for(var i = 1; i <= 8; i++){
+        //recall all integers i <= 8 are also less than 10, thus needing leading zero
+        var led_class_number = "led0" + i;
+        // orientation class to state whether led is horizontal or vertical
+            // if remainder when divided by 4 of i -1 is less than 2, orientation is horiz, else vert
+        var orientation_class;
+        if( (i - 1) % 4 < 2){
+            orientation_class = "outer_horiz";
+        }else{
+            orientation_class = "outer_vert";
+        }
+        var $led = $('<div>').addClass('led').addClass('outer').addClass(orientation_class).addClass(led_class_number);
+
+        var $ext = $('<div>').addClass('ext');
+        var $int = $('<div>').addClass('int');
+        
+        $led.append($ext, $int);
+        $char.append($led);
+    }
+
+    //create inner LEDs in character
+    for(var i = 9; i <= 16; i++){
+        // if i is 
+        var led_class_number = "led" + (i < 10 ? "0" : "") + i;
+        var orientation_class;
+        var $led = $('<div>').addClass('led').addClass('inner');
+
+        // class to state orientation of inner led
+            // if remainder when divided by 2 of i -1 is  equal to 0, orientation is horiz or vert, else is diagonal
+        if( (i-1) % 2 == 0){
+            // orientation is horiz or vert
+            if( (i-1) % 4 == 0){
+                // orientation is vert
+                orientation_class = "inner_vert";
+                var $left = $('<div>').addClass('left');
+                var $right = $('<div>').addClass('right');
+                $led.append($left, $right);
+            }else{
+                // orientation is horiz
+                orientation_class = "inner_horiz";
+                var $top = $('<div>').addClass('top');
+                var $bottom = $('<div>').addClass('bottom');
+                $led.append($top, $bottom);
+            }
+        }else{
+            // orientation is diag
+            orientation_class = "diag";
+            var $outer_end = $('<div>').addClass('outer_end');
+            var $middle_left = $('<div>').addClass('middle').addClass('left');
+            var $middle_right = $('<div>').addClass('middle').addClass('right');
+            var $inner_end = $('<div>').addClass('inner_end');
+            $led.append($outer_end, $middle_left, $middle_right, $inner_end);
+        }
+        $led.addClass(orientation_class).addClass(led_class_number);
+        $char.append($led);
     }
 }
