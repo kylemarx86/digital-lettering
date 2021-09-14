6ef542ba86418f7cdc4d0e9021dfdb42fc339693
kylemarx86 <kylemarx86@csu.fullerton.edu>
2021-09-13 21:57:34 -0700

    change references of letters to char.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 
 
 
+
 //letters array
 let char_arr = {
         "A": [true, true, true, true,
@@ -23,38 +24,37 @@ let char_arr = {
 //make letter funciton
 function make_char(char_to_write){
     if(char_to_write == "A"){
-        $('.letter_A .led').removeClass('unlit');
-        $('.letter_A .led05').addClass('unlit');
-        $('.letter_A .led06').addClass('unlit');
-        $('.letter_A .led09').addClass('unlit');
-        $('.letter_A .led10').addClass('unlit');
-        $('.letter_A .led12').addClass('unlit');
-        $('.letter_A .led13').addClass('unlit');
-        $('.letter_A .led14').addClass('unlit');
-        $('.letter_A .led16').addClass('unlit');
+        $('.char_A .led').removeClass('unlit');
+        $('.char_A .led05').addClass('unlit');
+        $('.char_A .led06').addClass('unlit');
+        $('.char_A .led09').addClass('unlit');
+        $('.char_A .led10').addClass('unlit');
+        $('.char_A .led12').addClass('unlit');
+        $('.char_A .led13').addClass('unlit');
+        $('.char_A .led14').addClass('unlit');
+        $('.char_A .led16').addClass('unlit');
     }else if (char_to_write == "B"){
-        $('.letter_B .led').removeClass('unlit');
-        $('.letter_B .led07').addClass('unlit');
-        $('.letter_B .led08').addClass('unlit');
-        $('.letter_B .led10').addClass('unlit');
-        $('.letter_B .led12').addClass('unlit');
-        $('.letter_B .led14').addClass('unlit');
-        $('.letter_B .led15').addClass('unlit');
-        $('.letter_B .led16').addClass('unlit');
+        $('.char_B .led').removeClass('unlit');
+        $('.char_B .led07').addClass('unlit');
+        $('.char_B .led08').addClass('unlit');
+        $('.char_B .led10').addClass('unlit');
+        $('.char_B .led12').addClass('unlit');
+        $('.char_B .led14').addClass('unlit');
+        $('.char_B .led15').addClass('unlit');
+        $('.char_B .led16').addClass('unlit');
     }else if(char_to_write == "X"){
-        $('.letter_X .led').removeClass('unlit');
-        $('.letter_X .led01').addClass('unlit');
-        $('.letter_X .led02').addClass('unlit');
-        $('.letter_X .led03').addClass('unlit');
-        $('.letter_X .led04').addClass('unlit');
-        $('.letter_X .led05').addClass('unlit');
-        $('.letter_X .led06').addClass('unlit');
-        $('.letter_X .led07').addClass('unlit');
-        $('.letter_X .led08').addClass('unlit');
-        $('.letter_X .led09').addClass('unlit');
-        $('.letter_X .led11').addClass('unlit');
-        $('.letter_X .led13').addClass('unlit');
-        $('.letter_X .led15').addClass('unlit');
+        $('.char_X .led').removeClass('unlit');
+        $('.char_X .led01').addClass('unlit');
+        $('.char_X .led02').addClass('unlit');
+        $('.char_X .led03').addClass('unlit');
+        $('.char_X .led04').addClass('unlit');
+        $('.char_X .led05').addClass('unlit');
+        $('.char_X .led06').addClass('unlit');
+        $('.char_X .led07').addClass('unlit');
+        $('.char_X .led08').addClass('unlit');
+        $('.char_X .led09').addClass('unlit');
+        $('.char_X .led11').addClass('unlit');
+        $('.char_X .led13').addClass('unlit');
+        $('.char_X .led15').addClass('unlit');
     }
 }
-
