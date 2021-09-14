0b5c39e11911b21220bcd7a45eb73be39888d6ef
kylemarx86 <kylemarx86@csu.fullerton.edu>
2021-09-13 21:41:41 -0700

    add functionality to write characters B and X.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     console.log('ready');
     make_char("A");
+    make_char("B");
+    make_char("X");
 });
 
 
@@ -30,6 +32,29 @@ function make_char(char_to_write){
         $('.letter_A .led13').addClass('unlit');
         $('.letter_A .led14').addClass('unlit');
         $('.letter_A .led16').addClass('unlit');
+    }else if (char_to_write == "B"){
+        $('.letter_B .led').removeClass('unlit');
+        $('.letter_B .led07').addClass('unlit');
+        $('.letter_B .led08').addClass('unlit');
+        $('.letter_B .led10').addClass('unlit');
+        $('.letter_B .led12').addClass('unlit');
+        $('.letter_B .led14').addClass('unlit');
+        $('.letter_B .led15').addClass('unlit');
+        $('.letter_B .led16').addClass('unlit');
+    }else if(char_to_write == "X"){
+        $('.letter_X .led').removeClass('unlit');
+        $('.letter_X .led01').addClass('unlit');
+        $('.letter_X .led02').addClass('unlit');
+        $('.letter_X .led03').addClass('unlit');
+        $('.letter_X .led04').addClass('unlit');
+        $('.letter_X .led05').addClass('unlit');
+        $('.letter_X .led06').addClass('unlit');
+        $('.letter_X .led07').addClass('unlit');
+        $('.letter_X .led08').addClass('unlit');
+        $('.letter_X .led09').addClass('unlit');
+        $('.letter_X .led11').addClass('unlit');
+        $('.letter_X .led13').addClass('unlit');
+        $('.letter_X .led15').addClass('unlit');
     }
 }
 
